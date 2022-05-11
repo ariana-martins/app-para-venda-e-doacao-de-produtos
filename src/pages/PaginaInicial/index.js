6557b211c8e11f1ff94b2c57ce4ec37a8f3be233
ariana-martins <arianaestrelar@gmail.com>
2022-05-11 20:50:12 -0300

    Funcionando Pagina Inicial em andamento

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -2,36 +2,23 @@ import React from 'react';
 import { View, Text, StyleSheet, Image, ScrollView, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 //import Pesquisar from '../Pesquisar';
-import NovosProdutos from '../../components/ListProdutos';
-
 
 export default function PaginaInicial(){
     return(
         <ScrollView 
          showsHorizontalScrollIndicator={false}
          style={{backgroundColor: '#FFFFFF'}} //cor na parte interna da página inicial
+         
         >
             <View style={styles.botaoPesquisar}> 
                 <View style={styles.inputArea}>
                     <Icon name="search" size={20} color="#000000" />
                     <TextInput
-                    placeholder="Pesquisar / Pesquise aqui..."
-                    style={styles.input}
+                        placeholder="Pesquisar / Pesquise aqui..."
+                        style={styles.input}
                     />
                 </View>
             </View>
-
-            <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ paddingHorizontal: 15}}>
-                <NovosProdutos
-                    cover={require('../../assets/logo.png')}
-                    name="Tênis branco"
-                    onPress={() => {}}
-                
-                />
-                
-
-            </ScrollView>
-
 
           <Text>Aqui vai a pagina inicial</Text>
           <Image style={styles.ListaItens} source={require('../../../src/assets/logo.png')} />
@@ -44,7 +31,7 @@ export default function PaginaInicial(){
 
 const styles = StyleSheet.create({
     botaoPesquisar:{
-        paddingHorizontal: 15, //distancia das bordas do input, lado esquero e direito
+        paddingHorizontal: 15, 
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
