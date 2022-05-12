35cefcfcedc02c5e1d46e229af41b5ad2c84548c
ariana-martins <arianaestrelar@gmail.com>
2022-05-12 13:14:21 -0300

    Pagina Inicial Card em andamento

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -1,34 +1,49 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, ScrollView, TextInput } from 'react-native';
+import { Card, Title } from 'react-native-paper';
 
 export default function PaginaInicial(){
     return(
-
+        
         //cor na parte interna da página inicial
-
-        <ScrollView style={{backgroundColor: '#FFFFFF'}}> 
-          
-          <Text>Aqui vai a pagina inicial</Text>
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img1.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img2.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img3.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img4.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img5.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img6.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img7.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img8.png')} />
-          <Image style={styles.ListaItens} source={require('../../../src/assets/img9.png')} />
-
+        <ScrollView > 
+          <View>
+            <Card style={{padding:20, margin:10, backgroundColor: "#eddfdf"}}>
+                <Card.Cover source={require('../../../src/assets/img1.png')} />
+                <Title>Sapato novo</Title>
+                <Text>R$120,00</Text>
+                <Card.Cover source={require('../../../src/assets/img2.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img3.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img4.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img5.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img6.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img7.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img8.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+                <Card.Cover source={require('../../../src/assets/img9.png')} />
+                <Title>Título produto aqui</Title>
+                <Text>R$0,00</Text>
+            </Card>
+          </View>
         </ScrollView>
     );
 }
 
 
 const styles = StyleSheet.create({   
-
-    ListaItens: {
-        width: 96,
-        height: 118,
-    }
+    
 });
 
